d2f1c6024958f8c88a47f019e9d33c2a3394af22
yismaw17 <yismawmulaw@yahoo.com>
2023-03-13 12:56:00 +0000

    task java

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const argv = process.argv;
-const num = parseInt(argv[2]);
-if (isNaN(num)) {
+// prints two arguments passed to it, in the following format: “ is ”
+
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${num}`);
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
